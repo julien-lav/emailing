ebc1a3c92528c40d515fc84003ee30b03da9a839
julien-lav <julien.laville@hotmail.fr>
2018-09-05 11:14:06 +0200

    'fc'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,27 @@
 module.exports = {
-  title: "Harry Potter",
-  description: "The description of the site.",
-  head: [["link", { rel: "icon", href: `/logo.png` }]],
-  base: "/",
-  dest: "./dist",
-
-  themeConfig: {
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Projects", link: "/projects/" },
-      { text: "Guide", link: "/guide/" },
-      { text: "GitHub", link: "https://github.com/mtobeiyf/vuepress-homepage" }
-    ],
-    sidebar: {
-      '/guide/': genSidebarConfig('Guide')
-    },
-    lastUpdated: 'Last Updated'
-  },
-
-  markdown: {
-    // options for markdown-it-anchor
-    anchor: { permalink: false },
-    config: md => {
-      md.use(require("markdown-it-katex"));
+    title: 'Scotch VuePress',
+    description: "A demo documentation using VuePress",
+    themeConfig:{
+        nav: [
+            { text: 'COUNTER', link: '/counter/' },
+            { text: 'GUIDE', link: '/guide/' },
+        ],
+        sidebar: [
+            {
+              title: 'Counter',
+              collapsable: false,
+              children: [
+                '/counter/counter-app'
+              ]
+            },
+            {
+              title: 'API Guide',
+              collapsable: false,
+              children: [
+                  '/guide/guide',
+                  '/guide/api'
+              ]
+            }
+          ]
     }
-  }
-};
-
-function genSidebarConfig (title) {
-  return [
-    {
-      title,
-      collapsable: false,
-      children: [
-        '',
-        'getting-started',
-        'customize',
-        'advanced',
-      ]
-    }
-  ]
 }
-
